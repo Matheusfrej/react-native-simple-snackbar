5f119e696145cb05fab4f0beb57dd584c56fcda3
Matheus Frej <matheusfrej@hotmail.com>
2023-05-04 11:28:28 -0300

    feat(main): first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-simple-snackbar';
+import { SnackBar, multiply } from 'react-native-simple-snackbar';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    multiply(2, 8).then(setResult);
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <SnackBar />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,135 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native';
+import { View } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+// export const setSnackBar = (
+//   flag: boolean,
+//   message: string,
+//   timer = 3000,
+//   position = 'top'
+// ) => {
+//   setFlag(flag);
+//   setMessage(message);
+//   setTimer(timer);
+//   setPosition(position);
+// };
+
+export function SnackBar() {
+  const [flag, setFlag] = useState<boolean | null>(true);
+  const [message, setMessage] = useState('Mensagem');
+  const [timer, setTimer] = useState(3000);
+  const [position, setPosition] = useState('top');
+
+  // useEffect(() => {
+  //   if (flag !== null) {
+  //     const timeout = setTimeout(() => {
+  //       setFlag(null);
+  //     }, timer);
+  //     return () => {
+  //       clearTimeout(timeout);
+  //     };
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [flag]);
+  return (
+    <View>
+      {flag !== null && position === 'top' && (
+        <View style={styles.containerTop}>
+          {flag && (
+            <View style={styles.successTop}>
+              <Text style={styles.content}>{message}</Text>
+            </View>
+          )}
+          {!flag && (
+            <View style={styles.failureTop}>
+              <Text style={styles.content}>{message}</Text>
+            </View>
+          )}
+        </View>
+      )}
+      {flag !== null && position === 'bottom' && (
+        <View style={styles.containerBottom}>
+          {flag && (
+            <View style={styles.successBottom}>
+              <Text style={styles.content}>{message}</Text>
+            </View>
+          )}
+          {!flag && (
+            <View style={styles.failureBottom}>
+              <Text style={styles.content}>{message}</Text>
+            </View>
+          )}
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerTop: {
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+  },
+
+  containerBottom: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+
+  successTop: {
+    position: 'absolute',
+    top: 64,
+    left: 32,
+    zIndex: 1,
+    borderRadius: 4,
+    padding: 16,
+    backgroundColor: '#22943E',
+  },
+
+  successBottom: {
+    position: 'absolute',
+    bottom: 64,
+    left: 32,
+    right: 32,
+    zIndex: 1,
+    borderRadius: 4,
+    padding: 16,
+    backgroundColor: '#22943E',
+  },
+
+  failureTop: {
+    position: 'absolute',
+    top: 64,
+    left: 32,
+    right: 32,
+    zIndex: 1,
+    borderRadius: 4,
+    padding: 16,
+    backgroundColor: '#E44040',
+  },
+
+  failureBottom: {
+    position: 'absolute',
+    bottom: 64,
+    left: 32,
+    right: 32,
+    zIndex: 1,
+    borderRadius: 4,
+    padding: 16,
+    backgroundColor: '#E44040',
+  },
+
+  content: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: '600',
+  },
+});
